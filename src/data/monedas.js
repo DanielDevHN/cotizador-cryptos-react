af900336cbe6ee00266bad265228401916a883f5
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-25 02:48:49 -0600

    Proyecto Finalizado

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar USD' },
+    {id: 'MXN', nombre: 'Pesos Mexicanos' },
+    {id: 'EUR', nombre: 'Euros' },
+    {id: 'GBP', nombre: 'Libras Esterlinas' },
+]
+
+export {
+    monedas
+}
